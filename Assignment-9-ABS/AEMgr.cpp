cdaac3f96e2680b2680d9b57f2dfaed823db48c5
chenlu20 <chen181396@163.com>
2025-12-29 14:09:06 +0800

    Update AEMgr.cpp

--- Assignment-9-ABS/AEMgr.cpp
+++ Assignment-9-ABS/AEMgr.cpp
@@ -594,350 +594,172 @@
 
 
 /**
- * AEMgr.cpp
+ * CFLR.cpp
  * @author kisslune 
  */
 
-#include "AEMgr.h"
+#include "A4Header.h"
 
 using namespace SVF;
+using namespace llvm;
+using namespace std;
 
-AEState AbstractExecutionMgr::test0()
+int main(int argc, char **argv)
 {
-    /*
-    // A simple example
+    auto moduleNameVec =
+            OptionBase::parseOptions(argc, argv, "Whole Program Points-to Analysis",
+                                     "[options] <input-bitcode...>");
 
-            int main() {
-                int* p;
-                int q;
-                int* r;
-                int x;
+    LLVMModuleSet::buildSVFModule(moduleNameVec);
 
-                p = malloc();
-                q = 5;
-                *p = q;
-                x = *p;
-                assert(x==10);
+    SVFIRBuilder builder;
+    auto pag = builder.build();
+    pag->dump("PAG");
+
+    CFLR solver;
+    solver.buildGraph(pag);
+    solver.solve();
+    solver.dumpResult();
+
+    LLVMModuleSet::releaseLLVMModuleSet();
+    return 0;
+}
+
+void CFLR::solve()
+{
+    std::unordered_set<unsigned> nodes;
+    
+    for (auto &p : graph->getSuccessorMap()) {
+        nodes.insert(p.first);
+        for (auto &q : p.second) {
+            for (unsigned d : q.second) {
+                nodes.insert(d);
+                workList.push(CFLREdge(p.first, d, q.first));
             }
-    */
-
-    AEState as;
-    NodeID p = getNodeID("p");
-    NodeID q = getNodeID("q");
-    NodeID r = getNodeID("r");
-    NodeID x = getNodeID("x");
-    NodeID malloc = getNodeID("malloc");
-    as[p] = AddressValue(getMemObjAddress("malloc"));
-    as[q] = IntervalValue(5, 5);
-    as.storeValue(p, as[q]);
-    as[x] = as.loadValue(p);
-    as.printAbstractState();
-    return as;
-}
-
-AEState AbstractExecutionMgr::test1()
-{
-    //    int main() {
-    //        int a;
-    //        int b;
-    //        a = 0;
-    //        b = a + 1;
-    //        assert(b>0);
-    //    }
-    AEState as;
-    NodeID a = getNodeID("a");
-    NodeID b = getNodeID("b");
-    as[a] = IntervalValue(0, 0);
-    as[b] = as[a].getInterval() + IntervalValue(1, 1);
-    as.printAbstractState();
-    return as;
-}
-
-AEState AbstractExecutionMgr::test2()
-{
-    //    int main() {
-    //        int* p;
-    //        int q;
-    //        int b;
-    //
-    //        p = malloc;
-    //        *p = 0;
-    //        q = *p;
-    //        *p = 3;
-    //        b = *p + 1;
-    //        assert(b>3);
-    //    }
-    AEState as;
-    NodeID p = getNodeID("p");
-    NodeID q = getNodeID("q");
-    NodeID b = getNodeID("b");
-    // TODO: put your code in the following braces
-    //@{
-    getNodeID("malloc");
-    as[p] = AddressValue(getMemObjAddress("malloc"));
-    as.storeValue(p, IntervalValue(0, 0));
-    as[q] = as.loadValue(p);
-    as.storeValue(p, IntervalValue(3, 3));
-    as[b] = as.loadValue(p).getInterval() + IntervalValue(1, 1);
-    //@}
-
-    as.printAbstractState();
-    return as;
-}
-
-AEState AbstractExecutionMgr::test3()
-{
-    //    int main() {
-    //        int** p;
-    //        int* q;
-    //        int* r;
-    //        int x;
-    //
-    //        p = malloc1(..);
-    //        q = malloc2(..);
-    //        *p = q;
-    //        *q = 10;
-    //        r = *p;
-    //        x = *r;
-    //        assert(x==10);
-    //    }
-    AEState as;
-    NodeID p = getNodeID("p");
-    NodeID q = getNodeID("q");
-    NodeID r = getNodeID("r");
-    NodeID x = getNodeID("x");
-    // TODO: put your code in the following braces
-    //@{
-    getNodeID("malloc1");
-    getNodeID("malloc2");
-    as[p] = AddressValue(getMemObjAddress("malloc1"));
-    as[q] = AddressValue(getMemObjAddress("malloc2"));
-    as.storeValue(p, as[q]);
-    as.storeValue(q, IntervalValue(10, 10));
-    as[r] = as.loadValue(p);
-    as[x] = as.loadValue(r);
-    //@}
-
-    as.printAbstractState();
-    return as;
-}
-
-AEState AbstractExecutionMgr::test4()
-{ //    int main() {
-    //        int* p;
-    //        int* x;
-    //        int* y;
-    //        int a;
-    //        int b;
-    //        p = malloc;
-    //        x = &p[0];
-    //        y = &p[1]
-    //        *x = 10;
-    //        *y = 11;
-    //        a = *x;
-    //        b = *y;
-    //        assert((a + b)>20);
-    AEState as;
-    NodeID p = getNodeID("p");
-    NodeID x = getNodeID("x");
-    NodeID y = getNodeID("y");
-    NodeID a = getNodeID("a");
-    NodeID b = getNodeID("b");
-    // TODO: put your code in the following braces
-    //@{
-    getNodeID("malloc");
-    as[p] = AddressValue(getMemObjAddress("malloc"));
-    as[x] = AddressValue(getGepObjAddress("malloc", 0));
-    as[y] = AddressValue(getGepObjAddress("malloc", 1));
-    as.storeValue(x, IntervalValue(10, 10));
-    as.storeValue(y, IntervalValue(11, 11));
-    as[a] = as.loadValue(x);
-    as[b] = as.loadValue(y);
-    //@}
-
-    as.printAbstractState();
-    return as;
-}
-
-AEState AbstractExecutionMgr::test5()
-{
-    //// Struct and pointers
-    //
-    //    struct A{ int f0; int* f1;};
-    //    int main() {
-    //       struct A* p;
-    //       int* x;
-    //       int* q;
-    //       int** r;
-    //       int* y;
-    //       int z;
-    //
-    //       p = malloc1;
-    //       x = malloc2;
-    //       *x = 5;
-    //       q = &(p->f0);
-    //       *q = 10;
-    //       r = &(p->f1);
-    //       *r = x;
-    //       y = *r;
-    //       z = *q + *y;
-    //       assert(z==15);
-    AEState as;
-    NodeID p = getNodeID("p");
-    NodeID x = getNodeID("x");
-    NodeID q = getNodeID("q");
-    NodeID r = getNodeID("r");
-    NodeID y = getNodeID("y");
-    NodeID z = getNodeID("z");
-    // TODO: put your code in the following braces
-    //@{
-    getNodeID("malloc1");
-    getNodeID("malloc2");
-    as[p] = AddressValue(getMemObjAddress("malloc1"));
-    as[x] = AddressValue(getMemObjAddress("malloc2"));
-    as.storeValue(x, IntervalValue(5, 5));
-    as[q] = AddressValue(getGepObjAddress("malloc1", 0));
-    as.storeValue(q, IntervalValue(10, 10));
-    as[r] = AddressValue(getGepObjAddress("malloc1", 1));
-    as.storeValue(r, as[x]);
-    as[y] = as.loadValue(r);
-    as[z] = as.loadValue(q).getInterval() + as.loadValue(y).getInterval();
-    //@}
-
-    as.printAbstractState();
-    return as;
-}
-
-AEState AbstractExecutionMgr::test6()
-{
-    //    int main(int arg) {  // arg is an interval  [4, 10]
-    //    int a;
-    //    int b;
-    //    a = arg + 1;
-    //    b = 5;
-    //    if(a > 10)
-    //       b = a;
-    //    assert(b>=5);
-    //    }
-    AEState as;
-    NodeID a = getNodeID("a");
-    NodeID b = getNodeID("b");
-    NodeID arg = getNodeID("arg");
-    // TODO: put your code in the following braces
-    //@{
-    as[arg] = IntervalValue(4, 10);
-    as[a] = as[arg].getInterval() + IntervalValue(1, 1);
-    as[b] = IntervalValue(5, 5);
-
-    // true branch: a > 10
-    AEState taken = as;
-    IntervalValue iv_taken = taken[a].getInterval();
-    iv_taken.meet_with(IntervalValue(11, 11));
-    taken[a] = iv_taken;
-    taken[b] = taken[a].getInterval();
-
-    // false branch: a <= 10
-    AEState not_taken = as;
-    IntervalValue iv_not_taken = not_taken[a].getInterval();
-    iv_not_taken.meet_with(IntervalValue(5, 10));
-    not_taken[a] = iv_not_taken;
-
-    // merge
-    as = taken;
-    as.joinWith(not_taken);
-    //@}
-
-    as.printAbstractState();
-    return as;
-}
-
-AEState AbstractExecutionMgr::test7()
-{
-    // int foo(int z) {
-    //     k = z;
-    //     return k;
-    // }
-    // int main() {
-    //   int x;
-    //   int y;
-    //   y = foo(2);
-    //   x = foo(3);
-    //   assert(x== 3 && y==2);
-    // }
-
-    AEState as;
-    NodeID x = getNodeID("x");
-    NodeID y = getNodeID("y");
-    // TODO: put your code in the following braces
-    //@{
-    NodeID k = getNodeID("k");
-    // call foo(2)
-    as[k] = IntervalValue(2, 2);
-    as[y] = as[k].getInterval();
-    // call foo(3)
-    as[k] = IntervalValue(3, 3);
-    as[x] = as[k].getInterval();
-    //@}
-
-    as.printAbstractState();
-    return as;
-}
-
-AEState AbstractExecutionMgr::test8()
-{
-    // int main() {
-    //    int x;
-    //    x=20;
-    //    while(x>0) {
-    //        x--;
-    //    }
-    //    assert(x == 0);
-    //    return 0;
-    // }
-
-    AEState entry_as;
-    AEState head_as;
-    AEState body_as;
-    AEState exit_as;
-    u32_t widen_delay = 3;
-    NodeID x = getNodeID("x");
-    // TODO: put your code in the following braces
-    //@{
-    // init entry
-    entry_as[x] = IntervalValue(20, 20);
-    head_as = entry_as;
-
-    // delay widening iterations
-    for (u32_t i = 0; i < widen_delay; i++) {
-        body_as = head_as;
-        body_as[x] = body_as[x].getInterval() + IntervalValue(-1, -1);
-
-        AEState new_head = entry_as;
-        new_head.joinWith(body_as);
-        head_as = new_head;
+        }
     }
-
-    // first widening
-    body_as = head_as;
-    body_as[x] = body_as[x].getInterval() + IntervalValue(-1, -1);
-
-    AEState new_head = entry_as;
-    new_head.joinWith(body_as);
-    head_as = head_as.widening(new_head);
-
-    // second widening
-    body_as = head_as;
-    body_as[x] = body_as[x].getInterval() + IntervalValue(-1, -1);
-
-    new_head = entry_as;
-    new_head.joinWith(body_as);
-    head_as = head_as.widening(new_head);
-
-    // loop exit
-    exit_as = head_as;
-    exit_as[x] = IntervalValue(0, 0);
-    //@}
-
-    exit_as.printAbstractState();
-    return exit_as;
+    
+    auto tryAdd = [this](unsigned s, unsigned d, EdgeLabel l) {
+        if (!graph->hasEdge(s, d, l)) {
+            graph->addEdge(s, d, l);
+            workList.push(CFLREdge(s, d, l));
+        }
+    };
+    
+    for (unsigned n : nodes) {
+        tryAdd(n, n, VF);
+        tryAdd(n, n, VFBar);
+        tryAdd(n, n, VA);
+    }
+    
+    while (!workList.empty()) {
+        auto e = workList.pop();
+        unsigned u = e.src, v = e.dst;
+        EdgeLabel L = e.label;
+        
+        auto &S = graph->getSuccessorMap();
+        auto &P = graph->getPredecessorMap();
+        
+        if (L == VFBar) {
+            if (S.count(v) && S[v].count(AddrBar))
+                for (unsigned t : S[v][AddrBar]) tryAdd(u, t, PT);
+            if (S.count(v) && S[v].count(VA))
+                for (unsigned t : S[v][VA]) tryAdd(u, t, VA);
+            if (S.count(v) && S[v].count(VFBar))
+                for (unsigned t : S[v][VFBar]) tryAdd(u, t, VFBar);
+        }
+        
+        if (L == AddrBar && P.count(u) && P[u].count(VFBar))
+            for (unsigned t : P[u][VFBar]) tryAdd(t, v, PT);
+        
+        if (L == Addr) {
+            if (S.count(v) && S[v].count(VF))
+                for (unsigned t : S[v][VF]) tryAdd(u, t, PTBar);
+        }
+        
+        if (L == VF) {
+            if (P.count(u) && P[u].count(Addr))
+                for (unsigned t : P[u][Addr]) tryAdd(t, v, PTBar);
+            if (S.count(v) && S[v].count(VF))
+                for (unsigned t : S[v][VF]) tryAdd(u, t, VF);
+            if (P.count(u) && P[u].count(VF))
+                for (unsigned t : P[u][VF]) tryAdd(t, v, VF);
+            if (P.count(u) && P[u].count(VA))
+                for (unsigned t : P[u][VA]) tryAdd(t, v, VA);
+        }
+        
+        if (L == Copy) tryAdd(u, v, VF);
+        if (L == CopyBar) tryAdd(u, v, VFBar);
+        
+        if (L == SV && S.count(v) && S[v].count(Load))
+            for (unsigned t : S[v][Load]) tryAdd(u, t, VF);
+        if (L == PV && S.count(v) && S[v].count(Load))
+            for (unsigned t : S[v][Load]) tryAdd(u, t, VF);
+        if (L == Load) {
+            if (P.count(u) && P[u].count(SV))
+                for (unsigned t : P[u][SV]) tryAdd(t, v, VF);
+            if (P.count(u) && P[u].count(PV))
+                for (unsigned t : P[u][PV]) tryAdd(t, v, VF);
+            if (P.count(u) && P[u].count(LV))
+                for (unsigned t : P[u][LV]) tryAdd(t, v, VA);
+        }
+        
+        if (L == Store) {
+            if (S.count(v) && S[v].count(VP))
+                for (unsigned t : S[v][VP]) tryAdd(u, t, VF);
+            if (S.count(v) && S[v].count(VA))
+                for (unsigned t : S[v][VA]) tryAdd(u, t, SV);
+        }
+        if (L == VP && P.count(u) && P[u].count(Store))
+            for (unsigned t : P[u][Store]) tryAdd(t, v, VF);
+        
+        if (L == VFBar) {
+            if (P.count(u) && P[u].count(VFBar))
+                for (unsigned t : P[u][VFBar]) tryAdd(t, v, VFBar);
+        }
+        
+        if (L == LoadBar) {
+            if (S.count(v) && S[v].count(SVBar))
+                for (unsigned t : S[v][SVBar]) tryAdd(u, t, VFBar);
+            if (S.count(v) && S[v].count(VP))
+                for (unsigned t : S[v][VP]) tryAdd(u, t, VFBar);
+            if (S.count(v) && S[v].count(VA))
+                for (unsigned t : S[v][VA]) tryAdd(u, t, LV);
+        }
+        if (L == SVBar && P.count(u) && P[u].count(LoadBar))
+            for (unsigned t : P[u][LoadBar]) tryAdd(t, v, VFBar);
+        if (L == VP && P.count(u) && P[u].count(LoadBar))
+            for (unsigned t : P[u][LoadBar]) tryAdd(t, v, VFBar);
+        
+        if (L == PV && S.count(v) && S[v].count(StoreBar))
+            for (unsigned t : S[v][StoreBar]) tryAdd(u, t, VFBar);
+        if (L == StoreBar && P.count(u) && P[u].count(PV))
+            for (unsigned t : P[u][PV]) tryAdd(t, v, VFBar);
+        
+        if (L == LV && S.count(v) && S[v].count(Load))
+            for (unsigned t : S[v][Load]) tryAdd(u, t, VA);
+        
+        if (L == VA) {
+            if (P.count(u) && P[u].count(VFBar))
+                for (unsigned t : P[u][VFBar]) tryAdd(t, v, VA);
+            if (S.count(v) && S[v].count(VF))
+                for (unsigned t : S[v][VF]) tryAdd(u, t, VA);
+            if (P.count(u) && P[u].count(Store))
+                for (unsigned t : P[u][Store]) tryAdd(t, v, SV);
+            if (S.count(v) && S[v].count(StoreBar))
+                for (unsigned t : S[v][StoreBar]) tryAdd(u, t, SVBar);
+            if (P.count(u) && P[u].count(PTBar))
+                for (unsigned t : P[u][PTBar]) tryAdd(t, v, PV);
+            if (S.count(v) && S[v].count(PT))
+                for (unsigned t : S[v][PT]) tryAdd(u, t, VP);
+            if (P.count(u) && P[u].count(LoadBar))
+                for (unsigned t : P[u][LoadBar]) tryAdd(t, v, LV);
+        }
+        
+        if (L == PTBar && S.count(v) && S[v].count(VA))
+            for (unsigned t : S[v][VA]) tryAdd(u, t, PV);
+        
+        if (L == PT && P.count(u) && P[u].count(VA))
+            for (unsigned t : P[u][VA]) tryAdd(t, v, VP);
+    }
 }
